1bb172b130935f626ff8bb018f44ee2a52606ca6
opujade <uripujade@gmail.com>
2024-03-11 18:30:49 +0100

    S2.1. Nivell 1 - Exercici 2

--- main.js
+++ main.js
@@ -5,14 +5,3 @@ const add = (a, b) => a + b;
 
 // Exercici 2
 const randomNumber = () => parseInt(Math.random() * 101);
-
-// Exercici 3
-class Person {
-    constructor(name) {
-        this._name = name;
-    }
-}
-
-const personaje = new Person('Antoni');
-
-const greet = () => console.log(`Hola `)
