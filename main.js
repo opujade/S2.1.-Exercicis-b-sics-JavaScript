cb29008f5a4bdedbf52182f2f324b6f905372caf
opujade <uripujade@gmail.com>
2024-03-12 11:02:04 +0100

    S2.1. Operador ternari Ex. 2

--- main.js
+++ main.js
@@ -7,31 +7,31 @@ const randomNumber = () => parseInt(Math.random() * 101);
 
 // Exercici 3
 class Person {
-    constructor(name) {
-        this._name = name;
-    }
+	constructor(name) {
+		this._name = name;
+	}
 
-    get name() {
-        return this._name;
-    }
+	get name() {
+		return this._name;
+	}
 
-    greet = () => console.log(this.name);
+	greet = () => console.log(this.name);
 }
 
 // Exercici 4
 const printNumbers = (arrayNumeros) => {
-    for (let i = 0; i < arrayNumeros.length; i++) {
-        console.log(arrayNumeros[i]);
-    }
-}
+	for (let i = 0; i < arrayNumeros.length; i++) {
+		console.log(arrayNumeros[i]);
+	}
+};
 
 // Exercici 5
 const message = () => {
-    setTimeout(() => {
-        console.log('Missatge')
-    }, 3000);
-}
+	setTimeout(() => {
+		console.log("Missatge");
+	}, 3000);
+};
 
 // Exercici 1.2. Operador ternari
 // Exercici 1
-const potConduir = (edat) => edat >= 18 ? 'Pots conduir' : 'No pots conduir';
+const potConduir = (edat) => (edat >= 18 ? "Pots conduir" : "No pots conduir");
